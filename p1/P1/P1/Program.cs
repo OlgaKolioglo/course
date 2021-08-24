139a323c28804008fda558cfe6dc7f2bf5129712
Olga <koliogloo@mail.ru>
2021-08-24 18:16:00 +0300

    .

---
+++ p1/P1/P1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 50;
+            int b = 10;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadKey();
+
+        }
+    }
+}
