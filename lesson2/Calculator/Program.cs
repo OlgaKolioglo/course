3e654aa93288b1cee8b0339b446d00a9eb79c3d3
Olga <koliogloo@mail.ru>
2021-08-26 17:54:52 +0300

    26/08

---
+++ lesson2/Calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Calculator
+{
+   
+    
+        class Program
+        {
+            static void Main(string[] args)
+            {
+            Console.WriteLine("Введите певрое число");
+                int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите знак");
+                string operation = Console.ReadLine();
+            Console.WriteLine("Введите второе число");
+                int num2 = int.Parse(Console.ReadLine());
+                int result = 0;
+                if (operation == "+")
+                {
+                    result = num1 + num2;
+                }
+                else if (operation == "-")
+                {
+                    result = num1 - num2;
+                }
+                else if (operation == "*")
+                {
+                    result = num1 * num2;
+                }
+                else if (operation == "/")
+                {
+                    result = num1 / num2;
+                }
+                else
+                {
+                    Console.WriteLine(" Oops...error");
+                }
+
+
+            Console.WriteLine("результат равен =");
+                  Console.WriteLine (result);
+
+            
+            }
+
+        }
+    }
+
